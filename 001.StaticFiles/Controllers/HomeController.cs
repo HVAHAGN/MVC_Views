51a6089537fc83ae14c50f66529e6baea4ab7f70
hvahagn <vhoagamo@gmail.com>
2020-11-23 12:45:55 +0400

    Add project files.

---
+++ 001.StaticFiles/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using _001.StaticFiles.Models;
+
+namespace _001.StaticFiles.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            PlainModel model = new PlainModel()
+            {
+                Company = "Microsoft",
+                Employees = 100000,
+                Salary = 10000
+            };
+
+            ViewData["totalSalary"] = model.Employees * model.Salary;
+            ViewData["company"] = model.Company;
+            ViewData["employees"] = model.Employees;
+            return View();
+        }
+    }
+}
---
+++ 002.CodeInsertions/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using _002.CodeInsertions.Models;
+
+namespace _002.CodeInsertions.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<PlainModel> models = new List<PlainModel>()
+            {
+                new PlainModel() {Company = "Microsoft", Employees = 100000, Salary = 10000},
+                new PlainModel() {Company = "Intel", Employees = 55000, Salary = 7000},
+                new PlainModel() {Company = "Google", Employees = 10000, Salary = 8000}
+            };
+
+            return View(models);
+        }
+    }
+}
